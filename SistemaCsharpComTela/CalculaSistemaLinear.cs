5f513da2f78ae564c4a78d7ebed2ddc5a8a4f334
Rubens silva lima <aralis.silva@hotmail.com>
2019-08-18 09:42:10 -0300

    code gauss

--- SistemaCsharpComTela/CalculaSistemaLinear.cs
+++ SistemaCsharpComTela/CalculaSistemaLinear.cs
@@ -5,8 +5,20 @@
 
         public double[] Resolucao(double[][] a, double[] b)
         {
+            double[] result = new double[b.Length];
+            result[b.Length] = b[b.Length] / a[b.Length][b.Length];
+            double s;
+            for (int i = b.Length; i >0; i--)
+            {
+                s = 0;
+                for (int j = i+1; j < b.Length; j++)
+                {
+                    s = s + a[i][j] * result[j];
+                }
+                result[i] = (b[i] - s) / a[i][i];
+            }    
 
-            return null;
+            return result;
 
         }
         public double[] Eliminacao(double[][] a, double[] b)
